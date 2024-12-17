48ca81cc55167fd360448c0f9fc4a062b525eff8
Croliv <108311920+Croliv@users.noreply.github.com>
2024-12-17 09:12:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function redirectToWhatsApp(event) {
+    event.preventDefault();
+    const phoneInput = document.getElementById('phoneNumber').value.trim();
+    const messageInput = document.getElementById('message').value.trim();
+
+    if (!phoneInput) {
+        alert('Por favor, insira um número de telefone.');
+        return;
+    }
+
+    const encodedMessage = encodeURIComponent(messageInput);
+    const whatsappLink = `https://wa.me/+55${phoneInput}?text=${encodedMessage}`;
+
+    window.open(whatsappLink, '_blank');
+}
